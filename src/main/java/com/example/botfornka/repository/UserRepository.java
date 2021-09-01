9856539ffc10f7ab2a7b423eefa85cdc50f027d7
Mikita <73610514+MikitaSerou@users.noreply.github.com>
2021-09-01 12:28:35 +0300

    Scheduled task for ping

--- src/main/java/com/example/botfornka/repository/UserRepository.java
+++ src/main/java/com/example/botfornka/repository/UserRepository.java
@@ -4,10 +4,14 @@ import com.example.botfornka.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByChatId(Long chatId);
 
     @Query(value = "SELECT is_active FROM users WHERE chat_id = ?", nativeQuery = true)
     Boolean getUserStatus(Long chatId);
+
+    List<User> findAllByIsActive(Boolean isActive);
 }
