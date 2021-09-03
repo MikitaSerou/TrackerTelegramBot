b329704bbf58ac2d7deb67755fff20a60fcf4832
Mikita <73610514+MikitaSerou@users.noreply.github.com>
2021-09-03 10:32:34 +0300

    Spring profiles, embeded db, Dockerfile

--- src/main/java/com/example/botfornka/service/impl/ScheduledPingService.java
+++ src/main/java/com/example/botfornka/service/impl/ScheduledPingService.java
@@ -33,7 +33,7 @@ public class ScheduledPingService {
     public void trackCurrentServerState() {
         try {
             throwExceptionIfServerError(restServiceImpl.doGetRequestByURL(URL));
-            notifyAboutRestored();
+            notifyIfConnectionRestored();
         } catch (ResourceAccessException | HttpServerErrorException exception) {
             log.error("CAN NOT GET RESPONSE FROM " + URL);
             notifyAboutLostConnection();
@@ -45,7 +45,7 @@ public class ScheduledPingService {
             throw new HttpServerErrorException(response.getStatusCode());
     }
 
-    private void notifyAboutRestored() {
+    private void notifyIfConnectionRestored() {
         if (!isActiveTask) {
             isActiveTask = true;
             log.info("-----------Connection restored-----------");
